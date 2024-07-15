15eed3c3e42ff7eea141c8e4cdcd8ede9b6288c4
ChristophLHR <11349102+ChristophLHR@users.noreply.github.com>
2024-07-15 08:27:19 +0200

    refactor: refactor helperFunctions

--- helperFunctions/helperFunctions.lua
+++ helperFunctions/helperFunctions.lua
@@ -1,6 +1,6 @@
 local helperFunctions = {}
 
-function helperFunctions:deepcopy(t)
+function helperFunctions.deepCopy(t)
     local u = {}
     for k, v in pairs(t) do u[k] = v end
     return setmetatable(u, getmetatable(t))
