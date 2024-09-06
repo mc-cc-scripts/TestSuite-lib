76267405252d8ec4aa7cc1932a0bcd41db7b4c54
ChristophLHR <11349102+ChristophLHR@users.noreply.github.com>
2024-09-06 19:18:32 +0200

    ci: added real Helperfunctions to the suit

--- helperFunctions/helperFunctions.lua
+++
@@ -1,9 +0,0 @@
-local helperFunctions = {}
-
-function helperFunctions.deepCopy(t)
-    local u = {}
-    for k, v in pairs(t) do u[k] = v end
-    return setmetatable(u, getmetatable(t))
-end
-
-return helperFunctions
